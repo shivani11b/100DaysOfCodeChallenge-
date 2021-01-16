e194d6ec2e9c35d54fbc9e0e0505533aab030c22
Shivani <62869416+shivani11b@users.noreply.github.com>
2021-01-16 20:34:22 +0530

    Create First.java

---
+++ First.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+public class MyFirst {
+
+    public static void main(String[] args) {
+       System.out.println("Hello World");
+       //System.out.println(args[0]);
+    }
+    
+}
